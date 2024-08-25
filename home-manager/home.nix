4304219d1f817ff36ae50f1f3902a829f5e7b450
DavidMazarro <davidmazarro98@gmail.com>
2024-08-25 12:03:45 +0200

    feat: added neofetch and obsidian packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -52,7 +52,11 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ 
+    neofetch
+    obsidian
+    # steam
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
