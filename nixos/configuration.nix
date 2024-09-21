31e89e11f233d1af35ca15bb92deb0ba1e5ca760
DavidMazarro <davidmazarro98@gmail.com>
2024-09-21 13:54:20 +0200

    feat(nixos): enabled Nvidia power management

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -127,6 +127,12 @@
     # Modesetting is REQUIRED (apparently).
     modesetting.enable = true;
 
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
     # Helps avoid screen tearing issues.
     forceFullCompositionPipeline = true;
 
