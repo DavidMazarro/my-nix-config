a27cf8842fa3f2feb821090b6beb266dea0613a6
DavidMazarro <davidmazarro98@gmail.com>
2024-09-22 11:50:29 +0200

    refactor(home): moved overlays to common.nix

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  outputs,
   ...
 }: {
   home.packages = with pkgs; [
@@ -16,4 +17,29 @@
     direnv
     lazygit
   ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
 }
