a3640ae0bd817a88c3a392aab12f2b6a183e0528
DavidMazarro <davidmazarro98@gmail.com>
2024-09-23 11:18:30 +0200

    feat(macos): imported more config from common.nix

--- home-manager/macos.nix
+++ home-manager/macos.nix
@@ -1,11 +1,12 @@
 {
   config,
   pkgs,
+  outputs,
   ...
 }: let
   username = "dmunuera";
   homeDirectory = "/Users/${username}";
-  common = import ./common.nix {inherit config pkgs;};
+  common = import ./common.nix {inherit config pkgs outputs homeDirectory;};
 in {
   nix = {
     package = pkgs.nix;
@@ -17,16 +18,21 @@ in {
     inherit homeDirectory;
   };
 
+  home.file = common.home.file;
+
+  home.shellAliases = common.home.shellAliases;
+
   home.packages = with pkgs;
     common.home.packages
     ++ [
       hello
     ];
 
+  nixpkgs = common.nixpkgs;
+
+  programs = common.programs;
+  
   # The state version is required and should stay at the version you
   # originally installed.
   home.stateVersion = "24.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
