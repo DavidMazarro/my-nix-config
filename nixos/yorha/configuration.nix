d4d55123071a0cefe88c8de6eb584ebca5655ba2
DavidMazarro <davidmazarro98@gmail.com>
2024-09-25 00:32:47 +0200

    feat(nixos): added fonts section and Noto fonts

--- nixos/yorha/configuration.nix
+++ nixos/yorha/configuration.nix
@@ -184,6 +184,12 @@
     wget
   ];
 
+  fonts.packages = with pkgs; [  
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
