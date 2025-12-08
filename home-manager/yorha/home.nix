7083bc54cd737ba4337213a71d06d47ab9259de8
DavidMazarro <davidmazarro98@gmail.com>
2025-12-08 01:47:34 +0100

    feat(yorha): set Rosé Pine as the GTK theme

--- home-manager/yorha/home.nix
+++ home-manager/yorha/home.nix
@@ -48,6 +48,20 @@
   };
 
   # GNOME / GTK settings
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "rose-pine";
+      package = pkgs.rose-pine-gtk-theme;
+    };
+
+    iconTheme = {
+      name = "rose-pine";
+      package = pkgs.rose-pine-icon-theme;
+    };
+  };
+
   dconf.settings = with lib.hm.gvariant; {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
